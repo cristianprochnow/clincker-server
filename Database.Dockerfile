78b0b6211ca665936645c4a122f3ed17670aba7b
Cristian Prochnow <cristian.prochnow@ascensus.com.br>
2024-04-03 13:03:49 -0300

    fix extra columns addiction in db sql migration file and fix volumes in database service from Docker Compose file

--- Database.Dockerfile
+++ Database.Dockerfile
@@ -1,3 +1,2 @@
 FROM mysql:5.7
 EXPOSE 3306
-COPY ./db/dump/ /docker-entrypoint-initdb.d/
--- db/dump/01_clincker_core.sql
+++ db/dump/01_clincker_core.sql
@@ -3,15 +3,16 @@ USE `clincker_core`;
 
 CREATE TABLE users (
    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   email VARCHAR(80) NULL DEFAULT NOT NULL,
+   email VARCHAR(80) NOT NULL,
    is_admin SET('0', '1') NULL DEFAULT '0',
-   password VARCHAR(80) COLLATE utf8_bin NOT NULL
+   password VARCHAR(80) NOT NULL,
+   created_at DATETIME DEFAULT CURRENT_TIMESTAMP NULL
 ) CHARACTER SET utf8 COLLATE utf8_bin;
 
-create unique index users_email_uindex
-    on users (email)
+CREATE UNIQUE INDEX users_email_uindex
+    ON users (email);
 
-INSERT INTO users (email, is_admin)
+INSERT INTO users (email, is_admin, password)
 VALUES
     ("contact@contact.com.br", "1", "swiheiwheweuwheuwhe"),
     ("extra@extra.com.br", "0", "hsuhuhuhsuhdushds");
