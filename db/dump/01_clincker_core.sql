c80eab419421e781bb79236156e2059d3fe75b5b
Cristian Prochnow <cristian.prochnow@ascensus.com.br>
2024-04-03 07:52:57 -0300

    add more column to Users table

--- db/dump/01_clincker_core.sql
+++ db/dump/01_clincker_core.sql
@@ -3,11 +3,15 @@ USE `clincker_core`;
 
 CREATE TABLE users (
    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   email VARCHAR(80) NULL DEFAULT NULL,
-   is_admin SET('0', '1') NULL DEFAULT '0'
+   email VARCHAR(80) NULL DEFAULT NOT NULL,
+   is_admin SET('0', '1') NULL DEFAULT '0',
+   password VARCHAR(80) COLLATE utf8_bin NOT NULL
 ) CHARACTER SET utf8 COLLATE utf8_bin;
+
+create unique index users_email_uindex
+    on users (email)
 
 INSERT INTO users (email, is_admin)
 VALUES
-    ("contact@contact.com.br", "1"),
-    ("extra@extra.com.br", "0");
+    ("contact@contact.com.br", "1", "swiheiwheweuwheuwhe"),
+    ("extra@extra.com.br", "0", "hsuhuhuhsuhdushds");
