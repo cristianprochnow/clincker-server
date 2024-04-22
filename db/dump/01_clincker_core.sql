e35a2ff3d46b4c887708f3d0ffba57622ee65a0c
Cristian Prochnow <cristian.prochnow@ascensus.com.br>
2024-04-22 13:26:31 -0300

    add tables for links in sql file

--- db/dump/01_clincker_core.sql
+++ db/dump/01_clincker_core.sql
@@ -1,19 +1,30 @@
 CREATE DATABASE `clincker_core` CHARACTER SET utf8 COLLATE utf8_bin;
 USE `clincker_core`;
 
-CREATE TABLE users (
-   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   email VARCHAR(80) NOT NULL,
-   name VARCHAR(100) NULL,
-   is_admin SET('0', '1') NULL DEFAULT '0',
-   password VARCHAR(80) NOT NULL,
-   created_at DATETIME DEFAULT CURRENT_TIMESTAMP NULL
-) CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE TABLE `users` (
+     `id` int(11) NOT NULL AUTO_INCREMENT,
+     `email` varchar(80) COLLATE utf8_bin NOT NULL,
+     `name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+     `is_admin` set('0','1') COLLATE utf8_bin DEFAULT '0',
+     `password` varchar(80) COLLATE utf8_bin NOT NULL,
+     `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `users_email_uindex` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE UNIQUE INDEX users_email_uindex
-    ON users (email);
-
-INSERT INTO users (email, name, is_admin, password)
-VALUES
-    ("contact@contact.com.br", "Contato par Contato", "1", "swiheiwheweuwheuwhe"),
-    ("extra@extra.com.br", "Extra Extra", "0", "hsuhuhuhsuhdushds");
+CREATE TABLE `links` (
+     `id` int(11) NOT NULL AUTO_INCREMENT,
+     `hash` varchar(64) COLLATE utf8_bin NOT NULL,
+     `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+     `edited_at` datetime DEFAULT NULL,
+     `original_url` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+     `domain` varchar(80) COLLATE utf8_bin DEFAULT NULL,
+     `resources` varchar(80) COLLATE utf8_bin DEFAULT NULL,
+     `protocol` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+     `user` int(11) NOT NULL,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `id_UNIQUE` (`id`),
+     UNIQUE KEY `hash_UNIQUE` (`hash`),
+     KEY `links_fk_users_idx` (`user`),
+     CONSTRAINT `links_fk_users` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
